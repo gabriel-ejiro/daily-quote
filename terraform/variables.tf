c7950b3450354aef605705056b2acf2407e09a1c
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-11-10 21:35:40 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,19 @@
-variable "project"  { type = string  default = "daily-quote" }
-variable "region"   { type = string  default = "eu-north-1" } # pick your region
-variable "schedule_utc_hour"   { type = number default = 8 }  # 08:00 UTC daily
-variable "schedule_utc_minute" { type = number default = 0 }
+variable "project" {
+  type    = string
+  default = "daily-quote"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "schedule_utc_hour" {
+  type    = number
+  default = 8
+}
+
+variable "schedule_utc_minute" {
+  type    = number
+  default = 0
+}
