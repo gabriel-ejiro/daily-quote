ecd8308d9794e4c59cacf8d85f93b2b8fc618be3
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-11-10 21:37:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,10 +37,16 @@ resource "aws_dynamodb_table" "quotes" {
 data "aws_iam_policy_document" "assume" {
   statement {
     effect = "Allow"
-    principals { type = "Service" identifiers = ["lambda.amazonaws.com"] }
-    actions   = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
 }
+
 
 resource "aws_iam_role" "lambda_role" {
   name               = "${local.name}-lambda-role"
